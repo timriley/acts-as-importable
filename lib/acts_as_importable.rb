69525f14a11258c4e0795c2e6fa1e161d1d22dfb
Tim Riley <tim@openmonkey.com>
2010-09-23 15:22:28 +1000

    some initial, incomplete, exploration into support composite primary keys. nb. none of this will work

--- lib/acts_as_importable.rb
+++ lib/acts_as_importable.rb
@@ -37,9 +37,25 @@ module Acts
         end
       end
 
-      def lookup(id)
-        lookup_class = read_inheritable_attribute(:importable_to) || "#{self.to_s.split('::').last}"
-        lookups[id] ||= Kernel.const_get(lookup_class).first(:conditions => {:legacy_id => id, :legacy_class => self.to_s}).try(:id__)
+      def lookup(*args)
+        lookup_class = Kernel.const_get(read_inheritable_attribute(:importable_to) || "#{self.to_s.split('::').last}")
+
+        lookups[id] ||= if lookup_class.column_names.include?('legacy_id_1')
+          
+          num_id_columns = 1
+          while legacy_class.column_names.include?("legacy_id_#{num_id_columns + 1}")
+            num_id_columns += 1
+          end
+          
+          cond_hash = {}
+          1.upto(num_id_colums) do |num|
+            cond_hash["legacy_id_#{num}"] = args[num -1]
+          end
+          
+          lookup_class.first(:conditions => cond_hash.merge{:legacy_class => self.to_s}).try(:id__)
+        else
+          lookup_class.first(:conditions => {:legacy_id => id, :legacy_class => self.to_s}).try(:id__)
+        end
       end
 
       def flush_lookups!
@@ -59,8 +75,16 @@ module Acts
       def import
         returning to_model do |new_model|
           if new_model
-            new_model.legacy_id     = self.id         if new_model.respond_to?(:"legacy_id=")
             new_model.legacy_class  = self.class.to_s if new_model.respond_to?(:"legacy_class=")
+            
+            if self.class.respond_to?(:primary_keys)
+              # The legacy class has composite primary keys
+              self.class.primary_keys.each_with_index do |key, index|
+                new_model.send(:"legacy_id_#{index+1}=", key.to_s) if new_model.respond_to?(:"legacy_id_#{index+1}=")
+              end
+            else
+              new_model.legacy_id = self.id if new_model.respond_to?(:"legacy_id=")
+            end
             
             if !new_model.save
               p new_model.errors
